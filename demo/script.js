9e1d19feb3ecb1a988eb78ced59d355bdfea7352
satindersharma <47383766+satindersharma@users.noreply.github.com>
2023-07-05 15:29:28 +0530

    js func

---
+++ demo/script.js
@@ -0,0 +1,47 @@
+console.log('window', window)
+console.log('document', document)
+
+
+// alert('are you sure')
+
+// const result = confirm('accept me')
+
+// console.log('result', result)
+
+// prompt('hello   ')
+
+
+
+// let h1 = document.getElementsByTagName('h1')
+// console.log('h1', h1)
+
+
+// let abc = document.getElementById('abc')
+// console.log('abc', abc)
+
+// let n = document.getElementsByClassName('new-cl')
+// console.log('abc', n)
+
+
+
+// .ddf{
+
+// }
+
+// #abc{
+    
+// }
+
+// let g = document.querySelector('h1')
+
+// console.log('g', g)
+
+// let y = document.querySelector('.container')
+
+// console.log('y', y)
+// console.log('y', y.innerText)
+// console.log('y', y.innerHTML)
+
+let z = document.querySelectorAll('.row')
+
+console.log('z', z)
---
+++ func.js
@@ -0,0 +1,103 @@
+// function ways
+
+// function_keyword function_name (arguments){
+//     function body
+// }
+// function func1();
+// const a;
+// func1()
+// var a;
+// console.log(a);
+
+
+// way 1
+// function func1(){
+
+//     console.log('func1')
+// }
+// func1()
+// var a = 10;
+// console.log(a);
+
+
+//1. function without parameter and without return
+// function func1(){
+
+//     console.log('func1')
+// }
+
+// func1()
+
+//2. // function with parameter and without return
+// function func1(name){
+
+//     console.log('hello', name)
+// }
+
+// func1('Sat')
+
+
+// //3. function without parameter and with return
+// function func1(){
+
+//     return 20;
+// }
+
+// let a = func1()
+// console.log(a);
+
+
+// //4. function with parameter and with return
+// function func1(a, b){
+
+//     return a + b;
+// }
+
+// let result = func1(10, 30)
+// console.log(result);
+
+
+// //4. function with default parameter and with return
+// function func1(a, b=10){
+
+//     return a + b;
+// }
+
+// let result = func1(10)
+// console.log(result);
+
+// // 5. function expression
+// const func1 = function(a,b){
+//     return a + b;
+// }
+
+// let result = func1(10,15)
+// console.log(result);
+
+//6. arrow function
+// const func1 = ()=>{
+//     console.log('arrow func')
+// }
+// func1()
+
+
+//7. arrow function // if ther is only one line, then you can ommit {}
+// const func1 = () => console.log('arrow func')
+
+// func1()
+
+
+//7. arrow function // if functin is returning in one line the we can ommit {}  
+//  as well as return keyword
+// const func1 = (a, b) => a + b;
+
+// const result = func1(1,2);
+// console.log(result);
+
+// func1()
+
+// 8. i there is only one parameter , then you can omit ()
+// const func1 = a => a*a;
+
+// const result = func1(10);
+// console.log(result);
